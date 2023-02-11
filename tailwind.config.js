17e8b36423011afc8f4cc6c47ce6fc4ac3c975db
Albert Tseng <albert.tseng@cardinalblue.com>
2023-02-11 17:07:32 +0800

    build(deps): add `tailwindcss`

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const colors = require('tailwindcss/colors');
+
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        primary: colors.yellow,
+      },
+    },
+  },
+  plugins: [],
+};
